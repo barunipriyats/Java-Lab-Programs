a0548710b685a1d3f6ec3b23990b51cef74c94e3
barunipriyats <68952987+barunipriyats@users.noreply.github.com>
2020-08-17 22:32:25 +0530

    Update from_miles_meter_to_kilometer.java

--- Lab_ex_4/to_kilometer/from_miles_meter_to_kilometer.java
+++ Lab_ex_4/to_kilometer/from_miles_meter_to_kilometer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package to_kilometer;
+package Lab_ex_4.to_kilometer;
 import java.util.Scanner;
 /**
  *
