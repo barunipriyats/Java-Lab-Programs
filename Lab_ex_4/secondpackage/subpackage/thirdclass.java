344acc4a9f33972b03fec7007b1302fd2167b9fc
barunipriyats <68952987+barunipriyats@users.noreply.github.com>
2020-08-17 22:26:22 +0530

    Update thirdclass.java

--- Lab_ex_4/secondpackage/subpackage/thirdclass.java
+++ Lab_ex_4/secondpackage/subpackage/thirdclass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package secondpackage.subpackage;
+package Lab_ex_4.secondpackage.subpackage;
 
 /**
  *
