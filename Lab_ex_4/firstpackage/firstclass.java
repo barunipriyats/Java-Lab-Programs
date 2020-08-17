ace916f5658e057c660262a848d1fd2dde25f4d1
barunipriyats <68952987+barunipriyats@users.noreply.github.com>
2020-08-17 20:59:08 +0530

    Add files via upload

---
+++ Lab_ex_4/firstpackage/firstclass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstpackage;
+
+/**
+ *
+ * @author Baruni Priya T S
+ */
+public class firstclass {
+    public void first()
+    {
+        System.out.println("Accessing of First package...");
+    }
+    
+    
+}
