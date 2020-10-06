4d3a7d45f2407c89dc00f70f6470d311bb51434e
barunipriyats <68952987+barunipriyats@users.noreply.github.com>
2020-10-06 20:10:11 +0530

    Add files via upload

---
+++ Lab_Ex_10/Copy_contents_from_one_file_to_another_file.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab_Ex_10;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Baruni Priya TS
+ */
+public class Copy_contents_from_one_file_to_another_file {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        FileInputStream fis=new FileInputStream("C:\\Users\\Baruni Priya TS\\Documents\\NetBeansProjects\\Unit1,2,3,4,5\\src\\Lab_Ex_10\\one.txt");
+        FileOutputStream fos=new FileOutputStream("C:\\Users\\Baruni Priya TS\\Documents\\NetBeansProjects\\Unit1,2,3,4,5\\src\\Lab_Ex_10\\two.txt");
+        int read;
+        while((read=fis.read())!=-1)//reads each character from the file "one.txt"
+        {
+            if(read==' ')//if space is encountered, skip the loop by use of "continue" statement
+                continue;
+            fos.write(read);//writinf each character to "two.txt" file
+        }
+        fis.close();
+        fos.close();
+    } 
+}
